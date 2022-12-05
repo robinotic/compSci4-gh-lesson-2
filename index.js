17208d955d0e1535d1b8a8e7d8add9299d79d033
robinotic <robin.hemuss@gmx.com>
2022-12-05 12:03:46 +0000

    Anything to do?

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 console.log("Phew, what a day!");
 console.log("And it's still only morning!");
 
-function() {
+function doh () {
     console.log("This will NEVER be printed!");
     console.log("You just watch!");
 }
